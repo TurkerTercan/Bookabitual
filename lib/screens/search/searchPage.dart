d5b01d3b673c4747193ddfddd02fa9cc859e8254
TurkerTercan <turker.tercan2017@gtu.edu.tr>
2020-11-22 19:35:14 +0300

    Muhammet Can Alkaşi created SearchPage he couldn't for some problems.

--- lib/screens/search/searchPage.dart
+++ lib/screens/search/searchPage.dart
@@ -1,8 +1,90 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController editingController = TextEditingController();
+
+  final duplicateItems = [
+    "Muhammetcan",
+    "Türker",
+    "Esra",
+    "Seyma",
+    "Seyda",
+    "Muhammedcan",
+    "Korku",
+    "Beyaz Diş",
+    "Hayvan Mezarlığı",
+    "Otomatik portakal"
+  ];
+  var items = List<String>();
+
+  @override
+  void initState() {
+    items.addAll(duplicateItems);
+    super.initState();
+  }
+
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = List<String>();
+    dummySearchList.addAll(duplicateItems);
+    if (query.isNotEmpty) {
+      List<String> dummyListData = List<String>();
+      dummySearchList.forEach((item) {
+        if (item.contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        items.clear();
+        items.addAll(duplicateItems);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              onChanged: (value) {
+                filterSearchResults(value);
+              },
+              controller: editingController,
+              decoration: InputDecoration(
+                  labelText: "Search User or Book",
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('${items[index]}'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
