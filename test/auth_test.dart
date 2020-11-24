eadfb638f3b07740054492d40b5c32d821cacccc
Şeyda Özer <seydaozer17@gmail.com>
2020-11-24 18:03:52 +0300

    Update auth_test.dart

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -2,47 +2,79 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:bookabitual/models/bookworm.dart';
 import 'package:bookabitual/states/currentUser.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class MockAuth extends Mock implements CurrentUser {}
-
-typedef Callback(MethodCall call);
-
-setupFirebaseAuthMocks([Callback customHandlers]) {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  MethodChannelFirebase.channel.setMockMethodCallHandler((call) async {
-
-    if (call.method == 'Firebase#initializeApp') {
-      return {
-        'name': call.arguments['appName'],
-        'options': call.arguments['options'],
-        'pluginConstants': {},
-      };
-    }
-
-    if (customHandlers != null) {
-      customHandlers(call);
-    }
-
-    return null;
-  });
-}
+class MockUser extends Mock implements Bookworm {}
+final MockUser _mockUser = MockUser();
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 void main() {
-  setupFirebaseAuthMocks();
-
-  setUpAll(() async {
-    await Firebase.initializeApp();
-  });
-  final MockAuth mockAuth = MockAuth();
+  final MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth();
+  final CurrentUser auth = CurrentUser();
+  setUp(() {});
+  tearDown(() {});
 
   test("emit occurs", () async {
-    expect(mockAuth.signUpUser("testing@testing.com", "testcase01", "Test User_01"), "Success");
+    expectLater(auth.getCurrentUser, emitsInOrder([MockUser]));
   });
+
+  test("sign up", () async {
+    when(
+        mockFirebaseAuth.createUserWithEmailAndPassword(
+            email: "test@gmail.com", password: "123456"),
+    ).thenAnswer((realInvocation) => null);
+
+    expect(await auth.signUpUser("test@gmail.com", "123456", "nickname"), "Success");
+  });
+
+  test("sign up exception", () async {
+    when(
+      mockFirebaseAuth.createUserWithEmailAndPassword(
+          email: "test@gmail.com", password: "123456"),
+    ).thenAnswer((realInvocation) =>
+      throw FirebaseAuthException(message: "You screwed up"));
+
+    expect(await auth.signUpUser("test@gmail.com", "123456", "nickname"), "You screwed up");
+  });
+
+  test("sign in", () async {
+    when(
+      mockFirebaseAuth.signInWithEmailAndPassword(
+          email: "test@gmail.com", password: "123456"),
+    ).thenAnswer((realInvocation) => null);
+
+    expect(await auth.loginUserWithEmail("test@gmail.com", "123456"), "Success");
+  });
+
+  test("sign in exception", () async {
+    when(
+      mockFirebaseAuth.signInWithEmailAndPassword(
+          email: "test@gmail.com", password: "123456"),
+    ).thenAnswer((realInvocation) =>
+    throw FirebaseAuthException(message: "You screwed up"));
+
+    expect(await auth.loginUserWithEmail("test@gmail.com", "123456"), "You screwed up");
+  });
+
+  test("sign out", () async {
+    when(
+      mockFirebaseAuth.signOut(),
+    ).thenAnswer((realInvocation) => null);
+
+    expect(await auth.signOut(), "Success");
+  });
+
+  test("sign out exception", () async {
+    when(
+      mockFirebaseAuth.signOut(),
+    ).thenAnswer((realInvocation) =>
+    throw FirebaseAuthException(message: "You screwed up"));
+
+    expect(await auth.signOut(), "You screwed up");
+  });
+
 }
