148670353c0a83c39452bb9f8bc1670b73c6c700
esraakaraa <32736315+esraakaraa@users.noreply.github.com>
2020-12-24 12:09:29 +0300

    Design of 'Add My Library' button is completed.

--- lib/screens/book/bookpage.dart
+++ lib/screens/book/bookpage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:bookabitual/screens/search/searchPage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../search/searchPage.dart';
+import 'dart:ui';
 
 
 class BookPage extends StatefulWidget {
@@ -12,194 +13,224 @@ class BookPage extends StatefulWidget {
   BookPage({Key key, @required this.booksdetail})
       : super(key: key);
 
-
   @override
   _BookPageState createState() => _BookPageState();
 }
 
 class _BookPageState extends State<BookPage> {
-
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home : Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).accentColor,
-            title: Text("bookabitual",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight:
-                FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Theme.of(context).bottomAppBarColor,
-              ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .accentColor,
+          title: Text("bookabitual",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight:
+              FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              color: Theme
+                  .of(context)
+                  .bottomAppBarColor,
             ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10),
-              ),
-            ),
-            centerTitle: true,
           ),
-          bottomNavigationBar: Container(
-            margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
-            height: 49,
-            color: Colors.transparent,
-            child: FittedBox(
-              child: ButtonTheme(
-                minWidth: 350.0,
-                height : 40.0,
-                child: RaisedButton(
-                  color:Colors.black87,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  onPressed: () {
-                    print('Eso');
-                        _onButtonPressed();
-                  },
-                  child: Row(
-                    children: [
-                      Text(
-                        'Add to Library',
-                        style: GoogleFonts.openSans(
-                            fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
             ),
-            /*child: FlatButton(
-              color: Colors.black,
-              onPressed: () {
-                print('esrafadswfa');
-              },
-              child: Text(
-                'Add to Library',
-                style: GoogleFonts.openSans(
-                    fontSize: 14, fontWeight: FontWeight.w600, color: Colors.white),
-              ),
-              shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            ),*/
           ),
-          body: SafeArea(
-            child: Container(
-              child: CustomScrollView(
-                slivers: <Widget>[
-                  SliverAppBar(
-                    backgroundColor: Colors.black,
-                    expandedHeight: MediaQuery.of(context).size.height * 0.5,
-                    flexibleSpace: Container(
-                      color: Color(0xFFFFD3B6),
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                            left: 25,
-                            top: 35,
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => SearchPage()),
-                                  // Navigator.pushReplacementNamed(context, "/searchPage",);
-                                );
-                              },
-
-                              child: Container(
-                                width: 32,
-                                height: 32,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: Colors.transparent),
-                                child: Icon(Icons.arrow_back),
-
-                              ),
-
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Container(
-                              margin: EdgeInsets.only(bottom: 62),
-                              width: 172,
-                              height: 225,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(
-                                  image: AssetImage("assets/profilePhoto15.png"),
-                                  //image: AssetImage("bookdetail.img"), normalde bunların çalışması lazım ama hata alıyorum
-
-                                ),
+          centerTitle: true,
+        ),
+        bottomNavigationBar: Container(
+          margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
+          height: 69,
+          color: Colors.transparent,
+          child: Stack(
+            children: [
+              ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                    margin: EdgeInsets.only(bottom: 5),
+                  );
+                },
+              ),
+              Positioned(
+                right: 10,
+                bottom: 10,
+                child: Container(
+                  height: 50,
+                  child: FittedBox(
+                    child: ButtonTheme(
+                      minWidth: 320,
+                      height: 40,
+                      child: RaisedButton(
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        onPressed: () => _onButtonPressed(),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Add My Library",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[200],
                               ),
                             ),
-                          )
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  SliverList(
-                      delegate: SliverChildListDelegate([
-                        Padding(
-                          padding: EdgeInsets.only(top: 24, left: 25),
-                          child:
-                          Text(
-                            'KORKU',
-                            //bookdetail.name,
-                            style: GoogleFonts.openSans(
-                                fontSize: 27,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 7, left: 25),
-                          child: Text(
-                            'GEORGE ORWELL',
-                            //booksdetail.author,
-                            style: GoogleFonts.openSans(
-                                fontSize: 14,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400),
-                          ),
-                        ),
-                      ]
-                      )
-                  )
-                ],
+                ),
               ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  backgroundColor: Colors.black,
+                  expandedHeight: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.5,
+                  flexibleSpace: Container(
+                    color: Color(0xFFFFD3B6),
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height * 0.5,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 25,
+                          top: 35,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SearchPage()),
+                                // Navigator.pushReplacementNamed(context, "/searchPage",);
+                              );
+                            },
+                            child: Container(
+                              width: 32,
+                              height: 32,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.transparent),
+                              child: Icon(Icons.arrow_back),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 62),
+                            width: 172,
+                            height: 225,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage("assets/profilePhoto15.png"),
+                                //image: AssetImage("bookdetail.img"), normalde bunların çalışması lazım ama hata alıyorum
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SliverList(
+                    delegate: SliverChildListDelegate([
+                      Padding(
+                        padding: EdgeInsets.only(top: 24, left: 25),
+                        child:
+                        Text(
+                          'KORKU',
+                          //bookdetail.name,
+                          style: GoogleFonts.openSans(
+                              fontSize: 27,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 7, left: 25),
+                        child: Text(
+                          'GEORGE ORWELL',
+                          //booksdetail.author,
+                          style: GoogleFonts.openSans(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ]
+                    )
+                )
+              ],
             ),
           ),
+        ),
       ),
     );
   }
+
+  void _onButtonPressed() {
+    showModalBottomSheet(context: context, builder: (context) {
+      return Container(
+        color: Colors.transparent,
+        height: 226.0,
+        child: Container(
+          child: _buildBottomNavigationMenu(),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(10),
+              topRight: const Radius.circular(10),
+            )
+          ),
+        ),
+      );
+    });
+  }
+
+  Column _buildBottomNavigationMenu(){
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.book),
+          title: Text('Finished'),
+          onTap: (){},
+        ),
+        ListTile(
+          leading: Icon(Icons.chrome_reader_mode),
+          title: Text('Reading'),
+          onTap: (){},
+        ),
+        ListTile(
+          leading: Icon(Icons.close_rounded),
+          title: Text('Unfinished'),
+          onTap: (){},
+        ),
+        ListTile(
+          leading: Icon(Icons.access_time),
+          title: Text('Will Read'),
+          onTap: (){},
+        ),
+      ],
+    );
+  }
 }
-
-void _onButtonPressed(){
-  print("Es");
-  ListTile(
-    leading: Icon(
-      Icons.favorite,
-      color: Colors.pink,
-      size: 24.0,
-      semanticLabel: 'Text to announce in accessibility modes',
-    ),
-    title: Text("Okuduklarım"),
-    onLongPress: (){
-      print('Okuduklarım butonuna basıldı.');
-    },
-    trailing: Icon(
-      Icons.favorite,
-      color: Colors.pink,
-      size: 24.0,
-      semanticLabel: 'Text to announce in accessibility modes',
-    ),
-  );
-}
-
-
-
-
