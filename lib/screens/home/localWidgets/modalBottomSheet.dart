4698814bbc4c71c9bd323e17439cf512903ee689
esraakaraa <32736315+esraakaraa@users.noreply.github.com>
2020-12-26 16:23:38 +0300

    ModelBottomSheet SingleChild is added by Türker tercan

--- lib/screens/home/localWidgets/modalBottomSheet.dart
+++ lib/screens/home/localWidgets/modalBottomSheet.dart
@@ -56,156 +56,158 @@ void onButtonPressed(
     builder: (context) {
       return Padding(
         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.66,
-          child: StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 10,),
-                  Container(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: ProjectContainer(child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Category: ", style: TextStyle(color: Colors.black, fontSize: 18),),
-                          DropdownButton<Item>(
-                            hint: Text("Choose a category"),
-                            value: _selectedItem,
-                            items: postChoice.map((Item item) {
-                              return DropdownMenuItem<Item>(
-                                child: Row(
-                                  children: [
-                                    item.icon,
-                                    SizedBox(width: 10,),
-                                    Text(item.name, style: TextStyle(color: Colors.black),),
-                                  ],
-                                ),
-                                value: item,
-                              );
-                            }).toList(),
-                            onChanged: (Item value) {
-                              setState(() {
-                                _selectedItem = value;
-                              });
-                            },
-                          ),
-                        ],
-                      )),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            child: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 10,),
+                    Container(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: ProjectContainer(child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Category: ", style: TextStyle(color: Colors.black, fontSize: 18),),
+                            DropdownButton<Item>(
+                              hint: Text("Choose a category"),
+                              value: _selectedItem,
+                              items: postChoice.map((Item item) {
+                                return DropdownMenuItem<Item>(
+                                  child: Row(
+                                    children: [
+                                      item.icon,
+                                      SizedBox(width: 10,),
+                                      Text(item.name, style: TextStyle(color: Colors.black),),
+                                    ],
+                                  ),
+                                  value: item,
+                                );
+                              }).toList(),
+                              onChanged: (Item value) {
+                                setState(() {
+                                  _selectedItem = value;
+                                });
+                              },
+                            ),
+                          ],
+                        )),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: ProjectContainer(child:
-                      TextFormField(
-                        controller: _isbn,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.book),
-                          hintText: "Book"
+                    SizedBox(height: 10,),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: ProjectContainer(child:
+                        TextFormField(
+                          controller: _isbn,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.book),
+                            hintText: "Book"
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: ProjectContainer(
-                      child: TextFormField(
-                        maxLines: 5,
-                        controller: _text,
-                        decoration: InputDecoration(
-                          alignLabelWithHint: true,
-                          prefixIcon: Icon(Icons.rate_review),
-                          hintText: _selectedItem == postChoice[0] ? "Quote" : "Review",
+                    SizedBox(height: 10,),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: ProjectContainer(
+                        child: TextFormField(
+                          maxLines: 5,
+                          controller: _text,
+                          decoration: InputDecoration(
+                            alignLabelWithHint: true,
+                            prefixIcon: Icon(Icons.rate_review),
+                            hintText: _selectedItem == postChoice[0] ? "Quote" : "Review",
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  _selectedItem == postChoice[1] ? Container(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: ProjectContainer(child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Rating: ", style: TextStyle(color: Colors.black, fontSize: 18),),
-                          DropdownButton<Rating>(
-                            hint: Text("Choose a rate"),
-                            value: _selectedRating,
-                            items: postRating.map((Rating item) {
-                              return DropdownMenuItem<Rating>(
-                                child: Row(
-                                  children: [
-                                    Icon(Icons.star, color: const Color(0xFF167F67),),
-                                    SizedBox(width: 10,),
-                                    Text(item.score, style: TextStyle(color: Colors.black),),
-                                  ],
-                                ),
-                                value: item,
-                              );
-                            }).toList(),
-                            onChanged: (Rating value) {
-                              setState(() {
-                                _selectedRating = value;
-                              });
-                            },
-                          ),
-                        ],
-                      )),
-                    ),
-                  ) : Container(),
-                  SizedBox(height: 10,),
-                  RaisedButton(
-                    onPressed: () async {
-                      String postID = Uuid().v4();
-                      if (_selectedItem == postChoice[1]) {
-                        ReviewPost temp = ReviewPost(
-                          isbn: _isbn.text,
-                          uid: Provider.of<CurrentUser>(context, listen: false).getCurrentUser.uid,
-                          postID: postID,
-                          text: _text.text,
-                          rating: _selectedRating.score,
-                          status: "Reading",
-                          likes: {},
-                          createTime: Timestamp.now(),
-                        );
-                        await temp.updateInfo();
-                        await BookDatabase().createReview(temp);
-                        viewState(() {
-                          postList.insert(0, temp);
-                        });
-                      } else {
-                        QuotePost temp = QuotePost(
-                          isbn: _isbn.text,
-                          uid: Provider.of<CurrentUser>(context, listen: false).getCurrentUser.uid,
-                          postID: postID,
-                          text: _text.text,
-                          status: "Reading",
-                          likes: {},
-                          createTime: Timestamp.now(),
-                        );
-                        await BookDatabase().createQuote(temp);
-                        await temp.updateInfo();
-                        viewState(() {
-                          postList.insert(0, temp);
-                        });
-                      }
-                      Navigator.pop(context);
-                      controller.animateTo(0.0, curve: Curves.bounceOut, duration: const Duration(milliseconds: 1000),);
-                    },
-                    color: Colors.purple[300],
-                    child: Text(
-                      "Create Post",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                  )
-                ],
-              );
-            },
+                    SizedBox(height: 10,),
+                    _selectedItem == postChoice[1] ? Container(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: ProjectContainer(child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Rating: ", style: TextStyle(color: Colors.black, fontSize: 18),),
+                            DropdownButton<Rating>(
+                              hint: Text("Choose a rate"),
+                              value: _selectedRating,
+                              items: postRating.map((Rating item) {
+                                return DropdownMenuItem<Rating>(
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.star, color: const Color(0xFF167F67),),
+                                      SizedBox(width: 10,),
+                                      Text(item.score, style: TextStyle(color: Colors.black),),
+                                    ],
+                                  ),
+                                  value: item,
+                                );
+                              }).toList(),
+                              onChanged: (Rating value) {
+                                setState(() {
+                                  _selectedRating = value;
+                                });
+                              },
+                            ),
+                          ],
+                        )),
+                      ),
+                    ) : Container(),
+                    SizedBox(height: 10,),
+                    RaisedButton(
+                      onPressed: () async {
+                        String postID = Uuid().v4();
+                        if (_selectedItem == postChoice[1]) {
+                          ReviewPost temp = ReviewPost(
+                            isbn: _isbn.text,
+                            uid: Provider.of<CurrentUser>(context, listen: false).getCurrentUser.uid,
+                            postID: postID,
+                            text: _text.text,
+                            rating: _selectedRating.score,
+                            status: "Reading",
+                            likes: {},
+                            createTime: Timestamp.now(),
+                          );
+                          await temp.updateInfo();
+                          await BookDatabase().createReview(temp);
+                          viewState(() {
+                            postList.insert(0, temp);
+                          });
+                        } else {
+                          QuotePost temp = QuotePost(
+                            isbn: _isbn.text,
+                            uid: Provider.of<CurrentUser>(context, listen: false).getCurrentUser.uid,
+                            postID: postID,
+                            text: _text.text,
+                            status: "Reading",
+                            likes: {},
+                            createTime: Timestamp.now(),
+                          );
+                          await BookDatabase().createQuote(temp);
+                          await temp.updateInfo();
+                          viewState(() {
+                            postList.insert(0, temp);
+                          });
+                        }
+                        Navigator.pop(context);
+                        controller.animateTo(0.0, curve: Curves.bounceOut, duration: const Duration(milliseconds: 1000),);
+                      },
+                      color: Colors.purple[300],
+                      child: Text(
+                        "Create Post",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
           ),
         ),
       );
